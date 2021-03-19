fe1ef55a8bc4c86024c9401cd2ea810a71f11d07
Edgar Duron <edgarduron@Edgars-MacBook-Pro.local>
2021-03-19 14:10:02 +0800

    Few changes, everything working now

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -3,16 +3,17 @@ module LuhnValidator
   # arguments: none
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
-
+  
   def validate_checksum
+    nums_a = number.to_s.chars.map(&:to_i)
+    sum = 0
     nums_a.reverse.map.with_index do |dig, ind|
       if ind.odd?
         dig *= 2
-        dig -= 9 if dig >=10
+        dig -= 9 if dig >= 10
       end
-      sum += dig
-      .to_s.chars.(&:to_i).sum
+      sum += dig.to_s.chars.map(&:to_i).sum
     end
-      sum.modulo(10).zero?
+    sum.modulo(10).zero?
   end
 end
